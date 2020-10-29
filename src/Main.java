fb557388dc3ef00867a9664f89e6b2baeb9b43c7
Olesea Gamurari <olesea.gamurari@endava.com>
2020-10-29 12:07:43 +0200

    Added 3 methods to Main class

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,11 @@ public class Main {
     public static void main(String[] args) {
      int x = 1;
     }
+
+    static void MyMethod() {
+        System.out.println("I created one method");
+    }
+    static void MySecondMethod() {
+        System.out.println("I created a second method");
+    }
 }
